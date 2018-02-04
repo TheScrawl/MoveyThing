610262d58f4ce88ddf26bc2a2f7d523bb274a23c
TheScrawl <the.scrawl.mail@gmail.com>
2018-02-04 19:43:07 +1300

    Rewrote to add instant keypress movements and screen clearing
    now only works on windows because of os.system('cls')

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import msvcrt
+import os
+
 r4 = ['.', '.', '.', '.', '.']
 r3 = ['.', '.', '.', '.', '.']
 r2 = ['.', '.', '.', '.', '.']
@@ -17,51 +20,70 @@ print(r0)
 
 
 def move():
-	global playerX
-	global playerY
-	choice = input(str('> '))
-	if choice == 'w':
-		try:
-			rows[playerY][playerX] = '.'
-			playerY = playerY + 1
-			rows[playerY][playerX] = 'x'
-		except IndexError:
-			playerY = 0
-			rows[playerY][playerX] = 'x'
+	while True:
+		global playerX
+		global playerY
+		if msvcrt.kbhit():
+			choice = msvcrt.getch().decode('ASCII')
+			if choice == 'w':
+				try:
+					rows[playerY][playerX] = '.'
+					playerY = playerY + 1
+					rows[playerY][playerX] = 'x'
+				except IndexError:
+					playerY = 0
+					rows[playerY][playerX] = 'x'
+				os.system('cls')
+				print(r4)
+				print(r3)
+				print(r2)
+				print(r1)
+				print(r0)
 
-	if choice == 's':
-		try:
-			rows[playerY][playerX] = '.'
-			playerY = playerY - 1
-			rows[playerY][playerX] = 'x'
-		except IndexError:
-			playerY = 4
-			rows[playerY][playerX] = 'x'
+			if choice == 's':
+				try:
+					rows[playerY][playerX] = '.'
+					playerY = playerY - 1
+					rows[playerY][playerX] = 'x'
+				except IndexError:
+					playerY = 4
+					rows[playerY][playerX] = 'x'
+				os.system('cls')
+				print(r4)
+				print(r3)
+				print(r2)
+				print(r1)
+				print(r0)
 
-	if choice == 'a':
-		try:
-			rows[playerY][playerX] = '.'
-			playerX = playerX - 1
-			rows[playerY][playerX] = 'x'
-		except IndexError:
-			playerX = 4
-			rows[playerY][playerX] = 'x'
+			if choice == 'a':
+				try:
+					rows[playerY][playerX] = '.'
+					playerX = playerX - 1
+					rows[playerY][playerX] = 'x'
+				except IndexError:
+					playerX = 4
+					rows[playerY][playerX] = 'x'
+				os.system('cls')
+				print(r4)
+				print(r3)
+				print(r2)
+				print(r1)
+				print(r0)
 
-	if choice == 'd':
-		try:
-			rows[playerY][playerX] = '.'
-			playerX = playerX + 1
-			rows[playerY][playerX] = 'x'
-		except IndexError:
-			playerX = 0
-			rows[playerY][playerX] = 'x'
-
-	print(r4)
-	print(r3)
-	print(r2)
-	print(r1)
-	print(r0)
-	move()
+			if choice == 'd':
+				try:
+					rows[playerY][playerX] = '.'
+					playerX = playerX + 1
+					rows[playerY][playerX] = 'x'
+				except IndexError:
+					playerX = 0
+					rows[playerY][playerX] = 'x'
+				os.system('cls')
+				print(r4)
+				print(r3)
+				print(r2)
+				print(r1)
+				print(r0)
 
 
 move()
